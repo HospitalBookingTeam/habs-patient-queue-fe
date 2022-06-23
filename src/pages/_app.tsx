1fb16335a0bec8cd7b0db4f5c774d17f145cb528
Le Nguyen Huu Quoc <huuquoc852@gmail.com>
2022-06-23 21:22:31 +0700

    Fix css bug (#1)
    Fixed first render css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,69 +17,69 @@ import Loading from '../components/Loading'
 const clientSideEmotionCache = createEmotionCache()
 
 interface MyAppProps extends AppProps {
-	emotionCache?: EmotionCache
+  emotionCache?: EmotionCache
 }
 
 const memoize = (fn: any) => {
-	let cache = {}
-	return (...args: any) => {
-		let n: keyof Object = args[0]
-		if (n in cache) {
-			return cache[n]
-		} else {
-			let result = fn(n)
-			cache[n] = result
-			return result
-		}
-	}
+  let cache = {}
+  return (...args: any) => {
+    let n: keyof Object = args[0]
+    if (n in cache) {
+      return cache[n]
+    } else {
+      let result = fn(n)
+      cache[n] = result
+      return result
+    }
+  }
 }
 
 // ignore in-browser next/js recoil warnings until its fixed.
 const mutedConsole = memoize((console: Console) => ({
-	...console,
-	warn: (...args: any) =>
-		args[0].includes('Duplicate atom key') ? null : console.warn(...args),
+  ...console,
+  warn: (...args: any) =>
+    args[0].includes('Duplicate atom key') ? null : console.warn(...args),
 }))
 
 global.console = mutedConsole(global.console)
 
 export default function MyApp(props: MyAppProps) {
-	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
-	const [loading, setLoading] = useState(true)
-	const router = useRouter()
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+  const [loading, setLoading] = useState(true)
+  const router = useRouter()
 
-	useEffect(() => {
-		router.events.on('routeChangeError', (e) => setLoading(false))
-		router.events.on('routeChangeStart', (e) => setLoading(true))
-		router.events.on('routeChangeComplete', (e) => {
-			setLoading(false)
-		})
+  useEffect(() => {
+    router.events.on('routeChangeError', (e) => setLoading(false))
+    router.events.on('routeChangeStart', (e) => setLoading(true))
+    router.events.on('routeChangeComplete', (e) => {
+      setLoading(false)
+    })
 
-		return () => {
-			router.events.off('routeChangeError', (e) => setLoading(false))
-			router.events.off('routeChangeStart', (e) => setLoading(true))
-			router.events.off('routeChangeComplete', (e) => setLoading(false))
-		}
-	}, [router.events])
+    return () => {
+      router.events.off('routeChangeError', (e) => setLoading(false))
+      router.events.off('routeChangeStart', (e) => setLoading(true))
+      router.events.off('routeChangeComplete', (e) => setLoading(false))
+    }
+  }, [router.events])
 
-	useEffect(() => {
-		const timeout = setTimeout(() => setLoading(false), 0)
-		return () => clearTimeout(timeout)
-	}, [])
+  useEffect(() => {
+    const timeout = setTimeout(() => setLoading(false), 0)
+    return () => clearTimeout(timeout)
+  }, [])
 
-	return (
-		<CacheProvider value={emotionCache}>
-			<Head>
-				<meta name="viewport" content="initial-scale=1, width=device-width" />
-			</Head>
-			<RecoilRoot>
-				<ThemeProvider theme={theme}>
-					{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-					<CssBaseline />
-					{loading && <Loading />}
-					<Component {...pageProps} />
-				</ThemeProvider>
-			</RecoilRoot>
-		</CacheProvider>
-	)
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          {loading && <Loading />}
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RecoilRoot>
+    </>
+  )
 }
